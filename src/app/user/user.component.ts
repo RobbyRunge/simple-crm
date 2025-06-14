0de2d749c9cc5896835c173b0993632ffcb19308
RobbyR98 <robbyrunge@yahoo.com>
2025-06-14 19:41:33 +0200

    feat: Add user button with tooltip and styling to user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
