260cafd727eb11ba37bcd9b0bbf92485b5ca2509
RobbyR98 <robbyrunge@yahoo.com>
2025-06-14 11:01:29 +0200

    feat: Refactor sidenav toggle to use mat-icon and update imports in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { CdkScrollable } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, CdkScrollable, MatSidenavModule],
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatButtonModule, MatIconModule, CdkScrollable],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
