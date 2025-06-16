7701c0737da388011e5c9c438175cdf5b9c53803
RobbyR98 <robbyrunge@yahoo.com>
2025-06-16 19:26:16 +0200

    feat: implement user data fetching and display in user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,24 +1,47 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDialog } from '@angular/material/dialog';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
-import { MatCardModule } from '@angular/material/card';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
-  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatDialogModule, MatCardModule],
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatCardModule,
+    CommonModule
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   dialog = inject(MatDialog);
   user = new User();
+  allUsers: any[] = [];
+  private firestore: Firestore = inject(Firestore);
+  users$: Observable<any[]> | undefined;
+
+  ngOnInit(): void {
+    const userCollection = collection(this.firestore, 'user');
+    this.users$ = collectionData(userCollection, { idField: 'id' });
+
+    this.users$.subscribe((changes: any[]) => {
+      this.allUsers = changes;
+    });
+  }
 
   openDialog() {
-    this.dialog.open(DialogAddUserComponent)
+    const dialogRef = this.dialog.open(DialogAddUserComponent);
+    dialogRef.afterClosed().subscribe;
   }
 }
