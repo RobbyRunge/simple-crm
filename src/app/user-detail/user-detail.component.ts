3f3b8f7b09acc16107cec7df07535b37ec371081
RobbyR98 <robbyrunge@yahoo.com>
2025-06-18 19:53:54 +0200

    feat: implement user detail display with dynamic data binding

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -10,11 +12,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserDetailComponent implements OnInit {
   userId = '';
+  user: User = new User();
 
   private route = inject(ActivatedRoute);
+  private firestore = inject(Firestore);
 
   ngOnInit() {
     this.userId = this.route.snapshot.paramMap.get('id') || '';
     console.log('GOT id', this.userId);
+    this.getUser();
+  }
+
+  getUser() {
+    const userDocRef = doc(this.firestore, 'user', this.userId);
+    docData(userDocRef).subscribe((user: any) => {
+      this.user = new User(user);
+      console.log('User data:', this.user);
+    });
   }
 }
