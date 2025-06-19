055afc32e79604fd1311d3e2fba268b9e45a4170
RobbyR98 <robbyrunge@yahoo.com>
2025-06-19 19:17:12 +0200

    feat: refactor app configuration to streamline Firebase integration and remove redundant providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+const firebaseConfig = {
+  projectId: "simple-crm-1a85d",
+  appId: "1:431971282660:web:a68b04094dda55350686f1",
+  storageBucket: "simple-crm-1a85d.firebasestorage.app",
+  apiKey: "AIzaSyA89EzA43h5SRNrcQ81CC8QSDOZ3ekUXqA",
+  authDomain: "simple-crm-1a85d.firebaseapp.com",
+  messagingSenderId: "431971282660"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideAnimations()
+    provideAnimations(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideNativeDateAdapter } from '@angular/material/core';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
-bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [
-    ...appConfig.providers,
-    provideNativeDateAdapter(), 
-    provideFirebaseApp(() => initializeApp({ 
-      projectId: "simple-crm-1a85d", 
-      appId: "1:431971282660:web:a68b04094dda55350686f1", 
-      storageBucket: "simple-crm-1a85d.firebasestorage.app", 
-      apiKey: "AIzaSyA89EzA43h5SRNrcQ81CC8QSDOZ3ekUXqA", 
-      authDomain: "simple-crm-1a85d.firebaseapp.com", 
-      messagingSenderId: "431971282660",
-    })), 
-    provideFirestore(() => getFirestore())
-  ]
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
